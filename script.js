392b875f36a41c01a892e79bce24b4e35b5421a3
Kinga Woch <kinga-niecko@o2.pl>
2023-03-11 11:01:32 +0100

    step-2 active states

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const form = document.querySelector("form");
-form.addEventListener("submit", (e) => {
-  if (!form.checkValidity()) {
-    e.preventDefault();
-  }
+// const form = document.querySelector("form");
+// form.addEventListener("submit", (e) => {
+//   if (!form.checkValidity()) {
+//     e.preventDefault();
+//   }
 
-  form.classList.add("was-validated");
-});
+//   form.classList.add("was-validated");
+// });
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) =>
+  card.addEventListener("click", () => {
+    if (card.classList.contains("active")) {
+      card.classList.toggle("active");
+    } else {
+      cards.forEach((card) => card.classList.remove("active"));
+      card.classList.add("active");
+    }
+  })
+);

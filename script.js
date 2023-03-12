f3ca0f830a33a65a1662dfbe61d67d617ce0f45d
Kinga Woch <kinga-niecko@o2.pl>
2023-03-12 11:16:23 +0100

    js

--- script.js
+++ script.js
@@ -1,44 +1,70 @@
-// const form = document.querySelector("form");
-// form.addEventListener("submit", (e) => {
-//   if (!form.checkValidity()) {
-//     e.preventDefault();
-//   }
+const name = document.getElementById("name");
+const nameErrorText = document.querySelector(".name-error-text");
+const email = document.getElementById("email");
+const emailErrorText = document.querySelector(".email-error-text");
+const phone = document.getElementById("phone");
+const phoneErrorText = document.querySelector(".phone-error-text");
 
-//   form.classList.add("was-validated");
-// });
+const nextStepBtn = document.querySelector(".btn-first-step");
 
-const cards = document.querySelectorAll(".card");
+// step 1
 
-cards.forEach((card) =>
-  card.addEventListener("click", () => {
-    if (card.classList.contains("active")) {
-      card.classList.toggle("active");
-    } else {
-      cards.forEach((card) => card.classList.remove("active"));
-      card.classList.add("active");
-    }
-  })
-);
+nextStepBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-// const inputFields = document.querySelectorAll(".input-field");
-// inputFields.forEach((inputField) =>
-//   inputField.addEventListener("click", () => {
-//     if (inputField.classList.contains("active")) {
-//       inputField.classList.remove("active");
-//     } else {
-//       inputField.classList.add("active");
-//     }
-//   })
-// );
-const inputFields = document.querySelectorAll(".input-field");
-const inputcheckbox = document.querySelectorAll(".form-check-input");
+  // name validation
 
-inputFields.forEach((inputField) =>
-  inputField.addEventListener("click", () => {
-    if (inputcheckbox.checked) {
-      inputField.classList.remove("active");
-    } else {
-      inputField.classList.add("active");
-    }
-  })
-);
+  let nameRexEx = /^[a-zA-Z\s]*$/;
+  if (!name.value) {
+    name.classList.add("invalid");
+    nameErrorText.innerText = "This field is required";
+  } else if (!name.value.match(nameRexEx)) {
+    name.classList.add("invalid");
+    nameErrorText.innerText = "Wrong format";
+  } else {
+    name.classList.remove("invalid");
+    nameErrorText.innerText = "";
+  }
+
+  // email validation
+
+  let emailRexEx = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
+
+  if (!email.value) {
+    email.classList.add("invalid");
+    emailErrorText.innerText = "This field is required";
+  } else if (!email.value.match(emailRexEx)) {
+    email.classList.add("invalid");
+    emailErrorText.innerText = "Wrong format";
+  } else {
+    email.classList.remove("invalid");
+    emailErrorText.innerText = "";
+  }
+
+  // phone validation
+
+  let phoneRexEx =
+    /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/;
+
+  if (!phone.value) {
+    phone.classList.add("invalid");
+    phoneErrorText.innerText = "This field is required";
+  } else if (!phone.value.match(phoneRexEx)) {
+    phone.classList.add("invalid");
+    phoneErrorText.innerText = "Wrong format";
+  } else {
+    phone.classList.remove("invalid");
+    phoneErrorText.innerText = "";
+  }
+
+  if (
+    name.value &&
+    name.value.match(nameRexEx) &&
+    email.value &&
+    email.value.match(emailRexEx) &&
+    phone.value &&
+    phone.value.match(phoneRexEx)
+  ) {
+    window.location.href = "step-2.html";
+  }
+});
